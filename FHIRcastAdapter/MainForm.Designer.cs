3a92399090c285b8fe6d58c750eb4a9964bac9bc
Martin Bellehumeur <martin_bellehumeur@yahoo.com>
2018-12-09 11:08:40 +0100

    test

--- FHIRcastAdapter/MainForm.Designer.cs
+++ FHIRcastAdapter/MainForm.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.hubURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSubscribe = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // hubURL
+            // 
+            this.hubURL.Location = new System.Drawing.Point(87, 22);
+            this.hubURL.Name = "hubURL";
+            this.hubURL.Size = new System.Drawing.Size(223, 20);
+            this.hubURL.TabIndex = 0;
+            this.hubURL.Text = "http://localhost:3000/api/hub/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HUB URL:";
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnSubscribe.Location = new System.Drawing.Point(357, 22);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(75, 23);
+            this.btnSubscribe.TabIndex = 2;
+            this.btnSubscribe.Text = "Subscribe";
+            this.btnSubscribe.UseVisualStyleBackColor = false;
+            this.btnSubscribe.Click += new System.EventHandler(this.btnSubscribe_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hubURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "FHIRcast Adapter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox hubURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubscribe;
     }
 }
 
--- FHIRcastAdapter/MainForm.cs
+++ FHIRcastAdapter/MainForm.cs
@@ -5,16 +5,81 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using WebSocketSharp;
+using System.IO;
 
 namespace FHIRcastAdapter
 {
     public partial class MainForm : Form
     {
+   
         public MainForm()
         {
             InitializeComponent();
         }
+
+        private void btnSubscribe_Click(object sender, EventArgs e)
+        {
+            string statusCode=subscribe(hubURL.Text);
+        }
+
+        static String subscribe(string url)
+        {
+            String hub_callback = "na";
+            String hub_mode = "subscribe";
+            String hub_events = "open-patient-chart";
+            String hub_secret = "secret";
+            String hub_topic = "DrXRay";
+            String hub_lease = "999";
+            String hub_channel_type = "websocket";
+            String hub_channel_endpoint = "endpointUID";
+
+            String result = "";
+            String strPost = "hub.callback=" + hub_callback + "&hub.mode=" + hub_mode + "&hub.events=" + hub_events;
+            strPost = strPost + "&hub.secret=" + hub_secret + "&hub.topic=" + hub_topic + "&hub.lease=" + hub_lease;
+            strPost = strPost + "&hub.channel.type=" + hub_channel_type + "&hub.channel.endpoint=" + hub_channel_endpoint;
+
+            StreamWriter myWriter = null;
+
+            HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(url);
+            objRequest.Method = "POST";
+            objRequest.ContentLength = strPost.Length;
+            objRequest.ContentType = "application/x-www-form-urlencoded";
+
+            try
+            {
+                myWriter = new StreamWriter(objRequest.GetRequestStream());
+                myWriter.Write(strPost);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            finally
+            {
+                myWriter.Close();
+            }
+
+            HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+            using (StreamReader sr =
+               new StreamReader(objResponse.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+
+                // Close and clean up the StreamReader
+                sr.Close();
+            }
+
+            string uriSocket = "ws://localhost:3000/bind/endpointUID";
+            var ws = new WebSocket(uriSocket);
+            ws.Connect();
+
+            return result;
+        }
+   
     }
 }
